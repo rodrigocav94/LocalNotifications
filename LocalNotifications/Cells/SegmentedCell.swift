0851b779ac431919f31c52e6b1072f8a5371aa94
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-07-02 18:06:13 -0300

    Added segmented control programmatically

--- LocalNotifications/Cells/SegmentedCell.swift
+++ LocalNotifications/Cells/SegmentedCell.swift
@@ -8,18 +8,34 @@
 import UIKit
 
 class SegmentedCell: UITableViewCell {
-
-    @IBOutlet var segmentedControl: UISegmentedControl!
+    var segmentedControl: UISegmentedControl!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
         self.selectionStyle = .none
-        segmentedControl.setTitle("Date", forSegmentAt: 0)
-        segmentedControl.setTitle("Interval", forSegmentAt: 1)
+        setupView()
     }
     
-    @IBAction func onValueChanged(_ sender: Any) {
+    func setupView() {
+        let horizontalPadding: CGFloat = 16
+        
+        let items = ["Date", "Interval"]
+        segmentedControl = UISegmentedControl(items: items)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.addTarget(self, action: #selector(onValueChanged), for: .valueChanged)
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(segmentedControl)
+        
+        NSLayoutConstraint.activate([
+            segmentedControl.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: horizontalPadding * -1),
+            segmentedControl.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: horizontalPadding),
+            segmentedControl.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
+    }
+    
+    @objc func onValueChanged(_ sender: Any) {
         
     }
 }
